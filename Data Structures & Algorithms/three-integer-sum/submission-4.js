d16a264a5269ae89b32c4a82c4d5f5737dfb0a1d
Piyush Gupta <31351605+piyush-98@users.noreply.github.com>
2026-05-10 02:02:13 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const n=nums.length
+        const result=[]
+        nums.sort((a,b)=>a-b);
+        for(let i=0;i<nums.length;i++){
+            if(i>0&&nums[i]===nums[i-1]){
+                continue;
+            }
+            if(nums[i]>0){
+                break;
+            }
+            let target=0-nums[i];
+            let j=i+1;
+            let k=n-1;
+            while(j<k){
+                let pairSum=nums[j]+nums[k]
+                if(pairSum===target){
+                    result.push([nums[i],nums[j],nums[k]])
+                    j++;
+                    k--;
+                    while(nums[k]===nums[k+1]){
+                        k--
+                    }
+                }
+                else if(pairSum<target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return result
+    }
+}
+
+[-2,0,0,2,2]
+
+
