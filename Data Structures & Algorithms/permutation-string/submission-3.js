d5caa3a012d37650f326ab935c34fbf9a1b8a778
Piyush Gupta <31351605+piyush-98@users.noreply.github.com>
2026-06-08 22:50:14 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        const s1Freq=Array(26).fill(0);
+        for(let ch of s1){
+            s1Freq[ch.charCodeAt(0) - 97]+=1;
+        }
+        let i=0;
+        let j=s1.length-1;
+        while(j<s2.length){
+            let tempFreq=Array(26).fill(0);
+            for(let k=i;k<=j;k++){
+                tempFreq[s2[k].charCodeAt(0)-97]+=1
+            }
+            let matched=true;
+            for(let k=0;k<26;k++){
+                if(tempFreq[k]!==s1Freq[k]){
+                    matched=false
+                    break;
+                }
+            }
+            if(matched){
+                console.log(i,j)
+                return true
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+}
+
+
+'lecacbaee'
+
